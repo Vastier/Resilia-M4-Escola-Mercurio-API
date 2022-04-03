8520f3286006b036e1943800408f9cec21730839
Vastier <iago.viana.dev@gmail.com>
2022-04-03 15:24:56 -0300

    feat: adiciona a página de index. Página que fica em '/' que redireciona para o Github da API.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import indexController from "./controllers/index-controller.js";
 
 const app = express()
+
+
+//Página principal usada como fallback redirecionando para o Github.
+indexController(app)
 
 export default app
---
+++ src/controllers/index-controller.js
@@ -0,0 +1,16 @@
+//Página de fallback na rota principal redirecionando para o repositório.
+
+const indexController = (app)=>{
+	app.get('/', (req, res)=>{
+		res.send(`
+		<p align="center">
+  			<img src="./src/assets/escola-mercurio-logo.png" alt="Logo da Escola Mercúrio" width="200px">
+		</p>
+		<h1 align="center">API da Escola Mercúrio - Entidade Funcionários</h1>
+		<h3 align="center">Link para mais informações: </h3>
+		<a style="margin:auto; text-align:center; display:block;" href="https://github.com/Vastier/Resilia-M4-Escola-Mercurio-API">Github da API</a>
+		`)
+	})
+}
+
+export default indexController
