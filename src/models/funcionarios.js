b14f092d61725d2b2c7a4f7b934776e6e62e3284
Vastier <iago.viana.dev@gmail.com>
2022-04-18 00:49:39 -0300

    refactor: Normaliza os erros da Model. Altera os erros dos métodos da Model Deixando as respostas mais informativas e consistentes entre si.

--- src/models/funcionarios.js
+++ src/models/funcionarios.js
@@ -19,11 +19,9 @@ class Funcionarios{
 			const resposta = await this.dao.listarTodosFuncionarios()
 			if (resposta.length > 0){
 				return resposta
-			} else {
-				throw new Error("Lista está vazia.")
-			}
+			} else throw "A Lista de Funcionários está vazia, use a rota '/funcionarios/inserir' caso queira inserir um funcionário."
 		} catch (error) {
-			throw new Error(error.message)
+			throw new Error(error)
 		}
 	}
 
@@ -36,7 +34,7 @@ class Funcionarios{
 
 			return await this.dao.insereFuncionario(funcionarioValidado)
 		} catch (error) {
-			throw error
+			throw new Error(error)
 		}
 	}
 	
@@ -45,9 +43,7 @@ class Funcionarios{
 			const resposta = await this.dao.buscaCpfFuncionario(cpf)
 			if (resposta.length > 0){
 				return resposta
-			} else {
-				throw ("Não foi encontrado funcionário com este CPF.")
-			}
+			} else throw "Não foi encontrado funcionário com este CPF."
 		} catch (error) {
 			throw new Error(error)
 		}
@@ -58,7 +54,7 @@ class Funcionarios{
 			const resposta = await this.dao.buscaCpfFuncionario(cpf)
 			if (resposta.length <= 0){
 				return undefined 
-			} else throw "CPF já foi cadastrado"
+			} else throw "Este CPF já foi cadastrado."
 		} catch (error) {
 			throw new Error(error)
 		}
