83915f3c72cf58fcb9c89d37f18877e2e31f54ce
Vastier <iago.viana.dev@gmail.com>
2022-04-23 13:53:54 -0300

    feat: Faz com que a API passe a usar `cors`.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from "express";
 import funcionariosController from "./controllers/funcionarios-controller.js";
 import indexController from "./controllers/index-controller.js";
 import db from './database/sqlite-db.js';
+import cors from 'cors'
 
 
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
+
 
 //Página principal usada como fallback redirecionando para o Github.
 indexController(app)
